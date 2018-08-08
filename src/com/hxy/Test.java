16592461f43be501db53312516629078f645509b
hxy <2548117197@qq.com>
2018-08-08 16:18:07 +0800

    local-hxy6

--- src/com/hxy/Test.java
+++ src/com/hxy/Test.java
@@ -12,8 +12,7 @@ public class Test {
 		System.out.println("gg");
 		System.out.println("qqqq");
 		System.out.println("werw");
-		System.out.println("wewtwetw");
-		System.out.println("gggg");
+
 		//local+zs4
 		//12314
 		//zs5
