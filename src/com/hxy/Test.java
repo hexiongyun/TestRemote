0d26e064c7685c5f2e78cbf4dac38b88316626da
hxy <2548117197@qq.com>
2018-08-08 14:25:20 +0800

    1

---
+++ src/com/hxy/Test.java
@@ -0,0 +1,14 @@
+package com.hxy;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author xiongyun.he
+ * @Date 2018/8/8 14:24
+ * @Version 1.0
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("gg");
+	}
+}
